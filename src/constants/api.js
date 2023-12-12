aa8215e76ee8c3949d2a2be9d35f0f72c3d7d21c
Sulfiya786 <sulfiya@unitdtechnologies.com>
2023-12-12 11:17:02 +0530

    api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,29 +1,16 @@
-import axios from 'axios'
+import axios from 'axios';
 
-// Define the base URL(s) conditionally
-// let baseURL;
+let baseURL;
 
-// if (process.env.NODE_ENV === 'production') {
-//   baseURL = 'http://43.228.126.245:5001';
-// } else {
-//   baseURL = 'http://localhost:5001';
-// }
+if (process.env.NODE_ENV === 'production') {
+  baseURL = 'http://43.228.126.245:4011';
+} else {
+  baseURL = 'http://43.228.126.245:4011';
+}
 
-// console.log('NODE_ENV:', process.env.NODE_ENV);
-// const api = axios.create({
-//   baseURL, // Use the baseURL variable here
-// });
-
+console.log('NODE_ENV:', process.env.NODE_ENV);
 const api = axios.create({
-baseURL: 'http://43.228.126.245:4011',
-//  baseURL: 'http://localhost:6001',
-
+  baseURL,
 });
 
-
-// const loginApi = axios.create({
-//   baseURL: 'https://art-cause.com:3003'
-// });
-
-
-export default api
+export default api;
