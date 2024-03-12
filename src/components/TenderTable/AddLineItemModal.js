1e5c6f2f5392632134cb855124eb8b7d9a748f4b
Sabina <sabina@unitdtechnologie.com>
2024-03-12 19:16:44 +0530

    addlineitems

--- src/components/TenderTable/AddLineItemModal.js
+++ src/components/TenderTable/AddLineItemModal.js
@@ -10,23 +10,23 @@ import {
   ModalHeader,
   ModalBody,
   ModalFooter,
-  Table,
-} from 'reactstrap';
+  
+  } from 'reactstrap';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import * as $ from 'jquery';
+//import Select from 'react-select';
 import random from 'random';
 import api from '../../constants/api';
 import message from '../Message';
 
-const AddLineItemModal = ({ addLineItemModal, setAddLineItemModal, projectInfo, quoteLine,quote}) => {
-  AddLineItemModal.propTypes = {
+const ViewLineItemModal = ({ addLineItemModal, setAddLineItemModal, projectInfo, quoteLine,quote }) => {
+  ViewLineItemModal.propTypes = {
     addLineItemModal: PropTypes.bool,
     setAddLineItemModal: PropTypes.func,
     projectInfo: PropTypes.any,
     quoteLine: PropTypes.any,
-    //selectedFormat:PropTypes.any,
-    quote:PropTypes.any,
+    quote: PropTypes.any,
   };
   //All state Varible
   const [totalAmount, setTotalAmount] = useState(0);
@@ -40,30 +40,60 @@ const AddLineItemModal = ({ addLineItemModal, setAddLineItemModal, projectInfo, 
       remarks: '',
       title: '',
       description: '',
+      asset_no: '',
+      no_of_days: '',
+      from_date: '',
+      to_date: '',
     },
   ]);
+  const [unitdetails, setUnitDetails] = useState();
+// Fetch data from API
+  const getUnit = () => {
+    api.get('/product/getUnitFromValueList', unitdetails)
+      .then((res) => {
+        const items = res.data.data
+        const finaldat = []
+        items.forEach(item => {
+          finaldat.push({ value: item.value, label: item.value })
+        })
+        setUnitDetails(finaldat)
+      })
+  }
+  //onchange function
+  // const onchangeItem = (selectedValue) => {
+  //   const updatedItems = addLineItem.map((item) => {
+  //     if (item.unit === selectedValue.value) {  // Compare with selectedValue.value
+  //       return {
+  //         ...item,
+  //         unit: selectedValue.value,  // Update the unit with the selected option's value
+  //         value: selectedValue.value  // Update the value with the selected option's value
+  //       };
+  //     }
+  //     return item;
+  //   });
+  
+  //   setAddLineItem(updatedItems);
+  // };
   //Insert Invoice Item
   const addLineItemApi = (obj) => {
     obj.opportunity_id = projectInfo;
     obj.quote_id = quoteLine;
     if (obj.title !== '' && obj.unit_price !== '' && obj.quantity !== '') {
       api
-      .post('/tender/insertQuoteItems', obj)
-      .then(() => {
-        message('Line Item Added Successfully', 'sucess');
-        setAddLineItemModal(false);
-         setTimeout(() => {
+        .post('/tender/insertQuoteItems', obj)
+        .then(() => {
+          message('Line Item Added Successfully', 'sucess');
+          setAddLineItemModal(false);
+          setTimeout(() => {
             window.location.reload();
           }, 1000);
-      })
-      .catch(() => {
-        message('Cannot Add Line Items', 'error');
-      });
-    }
-    else {
+        })
+        .catch(() => {
+          message('Cannot Add Line Items', 'error');
+        });
+    } else {
       message('Please fill all required fields', 'warning');
     }
-  
   };
   //Add new line item
   const AddNewLineItem = () => {
@@ -78,17 +108,17 @@ const AddLineItemModal = ({ addLineItemModal, setAddLineItemModal, projectInfo, 
         amount: '',
         title: '',
         description: '',
-        asset_no:'',
-        no_of_days:'',
-        from_date:'',
-        to_date:'',
+        asset_no: '',
+      no_of_days: '',
+      from_date: '',
+      to_date: '',
       },
     ]);
   };
   //Invoice item values
   const getAllValues = () => {
     const result = [];
-    $('.lineitem tbody tr').each(function input() {
+    $('.display tbody tr').each(function input() {
       const allValues = {};
       $(this)
         .find('input')
@@ -105,12 +135,11 @@ const AddLineItemModal = ({ addLineItemModal, setAddLineItemModal, projectInfo, 
     });
     console.log(result);
   };
-  
   //Invoice Items Calculation
   const calculateTotal = () => {
     let totalValue = 0;
     const result = [];
-    $('.lineitem tbody tr').each(function input() {
+    $('.display tbody tr').each(function input() {
       const allValues = {};
       $(this)
         .find('input')
@@ -142,9 +171,14 @@ const AddLineItemModal = ({ addLineItemModal, setAddLineItemModal, projectInfo, 
       setTotalAmount(finalTotal);
     }
   };
+
+  React.useEffect(() => {
+    getUnit();
+  }, []);
+
   return (
     <>
-      <Modal size="xl" isOpen={addLineItemModal} >
+      <Modal size="xl" isOpen={addLineItemModal}>
         <ModalHeader>
           Add Quote Items
           <Button
@@ -157,13 +191,13 @@ const AddLineItemModal = ({ addLineItemModal, setAddLineItemModal, projectInfo, 
             X
           </Button>
         </ModalHeader>
-        <ModalBody>    
+        <ModalBody>
           <Row>
             <Col md="12">
               <Form>
                 <Row>
                   <Row>
-                    <Col md="3" className="mb-4">
+                    <Col md="3">
                       <Button
                         className="shadow-none"
                         color="primary"
@@ -179,28 +213,23 @@ const AddLineItemModal = ({ addLineItemModal, setAddLineItemModal, projectInfo, 
                   {/* Invoice Item */}
                   {/* <Card> */}
                   <Card className="shadow-none overflow-auto updateOTModalTableCard">
-                    <Table className="display">
+                    <table className="display">
                       <thead>
                         <tr>
-                        
-                          <th scope="col">Title <span className="required"> *</span>{' '}</th>
+                          <th scope="col">Title </th>
                           <th scope="col">Description </th>
-                          <th scope="col">Unit</th>
-                          <th scope="col">Qty <span className="required"> *</span>{' '}</th>
-                          <th scope="col">Unit Price <span className="required"> *</span>{' '}</th>
+                          <th scope="col">Unit </th>
+                          <th scope="col">Qty</th>
+                          <th scope="col">Unit Price</th>
                           <th scope="col">Amount</th>
-                          
-                          
-
                           {quote.quote_format === 'format4' && (
                             <>
-                          <th scope="col">Asset No </th>
-                          <th scope="col">From Date </th>
-                          <th scope="col">To Date </th>
-                          <th scope="col">No of days </th>
-                          </>
+                              <th scope="col">Asset No </th>
+                              <th scope="col">From Date </th>
+                              <th scope="col">To Date </th>
+                              <th scope="col">No of days </th>
+                            </>
                           )}
-                          
                           <th scope="col">Remarks</th>
                           <th scope="col"></th>
                         </tr>
@@ -210,57 +239,98 @@ const AddLineItemModal = ({ addLineItemModal, setAddLineItemModal, projectInfo, 
                           addLineItem.map((item) => {
                             return (
                               <tr key={item.id}>
-                                
                                 <td data-label="Title">
-                                  <Input Value={item.title} className="w-auto"  type="text" name="title"  />
+                                  <Input Value={item.title} type="text" name="title" className="w-auto" />
                                 </td>
-                                
                                 <td data-label="Description">
-                                  <Input Value={item.description} className="w-auto"  type="text" name="description"  />
+                                  <Input Value={item.description} type="text" name="description" className="w-auto" />
                                 </td>
                                 <td data-label="Unit">
-                                  <Input Value={item.unit} className="w-auto" type="text" name="unit" />
+                                <Input
+                                
+                                name="unit" 
+                                className="w-auto"
+  
+/>
+
+
+                                  
+                                {/* <Input
+                  type="select"
+                  name="unit"
+                  onChange={handleInputs}
+                  value={item && item.unit}
+                >
+                  <option defaultValue="selected">Please Select</option>
+                  {unitdetails &&
+                    unitdetails.map((ele) => {
+                      return (
+                        <option key={ele.value} value={ele.value}>
+                          {ele.value}
+                        </option>
+                      );
+                    })}
+                </Input> */}
+                                  {/* <Input Value={item.unit} type="text" name="unit" /> */}
                                 </td>
                                 <td data-label="Qty">
-                                  <Input Value={item.quantity} className="w-auto"  type="number" name="quantity" />
+                                  <Input Value={item.quantity} type="number" name="quantity" className="w-auto"/>
                                 </td>
                                 <td data-label="Unit Price">
                                   <Input
                                     Value={item.unit_price}
-                                    className="w-auto" 
                                     onBlur={() => {
                                       calculateTotal();
                                     }}
                                     type="number"
                                     name="unit_price"
-                                    
+                                    className="w-auto"
                                   />
                                 </td>
-                                
                                 <td data-label="Amount">
-                                  <Input Value={item.amount} className="w-auto"  type="text" name="amount" disabled />
+                                  <Input Value={item.amount} type="text" name="amount" className="w-auto" disabled />
                                 </td>
-                                {quote.quote_format === "format4" && (
-                            <>
-                            <td data-label="Asset No">
-                                  <Input Value={item.asset_no} className="w-auto"  type="text" name="asset_no"  />
-                                </td>
-                                <td data-label="From Date">
-                                  <Input Value={item && moment(item.from_date).format('DD-MM-YYYY')} className="w-auto"  type="date" name="from_date"  />
-                                </td>
-                                <td data-label="To Date">
-                                  <Input Value={item && moment(item.to_date).format('DD-MM-YYYY')} className="w-auto"  type="date" name="to_date"  />
-                                </td>
-                                <td data-label="No of Days">
-                                  <Input Value={item.no_of_days} className="w-auto"  type="text" name="no_of_days"  />
-                                </td>
-                                </>
+                                {quote.quote_format === 'format4' && (
+                                  <>
+                                    <td data-label="Asset No">
+                                      <Input
+                                        Value={item.asset_no}
+                                        className="w-auto"
+                                        type="text"
+                                        name="asset_no"
+                                      />
+                                    </td>
+                                    <td data-label="From Date">
+                                      <Input
+                                        Value={item && moment(item.from_date).format('DD-MM-YYYY')}
+                                        className="w-auto"
+                                        type="date"
+                                        name="from_date"
+                                      />
+                                    </td>
+                                    <td data-label="To Date">
+                                      <Input
+                                        Value={item && moment(item.to_date).format('DD-MM-YYYY')}
+                                        className="w-auto"
+                                        type="date"
+                                        name="to_date"
+                                      />
+                                    </td>
+                                    <td data-label="No of Days">
+                                      <Input
+                                        Value={item.no_of_days}
+                                        className="w-auto"
+                                        type="text"
+                                        name="no_of_days"
+                                      />
+                                    </td>
+                                  </>
                                 )}
                                 <td data-label="Remarks">
-                                  <Input Value={item.remarks} className="w-auto"  type="text" name="remarks" />
+                                  <Input Value={item.remarks} type="text" name="remarks" className="w-auto"/>
                                 </td>
                                 <td data-label="Action">
-                                  
+                                 
                                     <Input type="hidden" name="id" Value={item.id}></Input>
                                     <span className='addline'
                                       onClick={() => {
@@ -274,7 +344,7 @@ const AddLineItemModal = ({ addLineItemModal, setAddLineItemModal, projectInfo, 
                             );
                           })}
                       </tbody>
-                    </Table>
+                    </table>
                   </Card>
                   <ModalFooter>
                     <Button
@@ -285,7 +355,7 @@ const AddLineItemModal = ({ addLineItemModal, setAddLineItemModal, projectInfo, 
                       }}
                     >
                       {' '}
-                      Save & Continue{' '}
+                      Submit{' '}
                     </Button>
                     <Button
                       className="shadow-none"
@@ -304,6 +374,7 @@ const AddLineItemModal = ({ addLineItemModal, setAddLineItemModal, projectInfo, 
         </ModalBody>
       </Modal>
     </>
+
   );
 };
-export default AddLineItemModal;
+export default ViewLineItemModal;
